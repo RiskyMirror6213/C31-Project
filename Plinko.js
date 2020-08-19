7c03ec9d24de2f42aa7439008e1937c2db23f33c
RiskyMirror6213 <53006341+RiskyMirror6213@users.noreply.github.com>
2020-08-19 18:56:52 +0530

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,17 @@
+class Plinko{
+    constructor(x, y, r){
+        var options = {
+            isStatic: true
+        }
+        this.r = r;
+    
+        this.body = Bodies.circle(x, y, this.r, options);
+        World.add(world, this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        fill(255, 255, 255);
+        ellipseMode(RADIUS);
+        ellipse(pos.x, pos.y, this.r);
+    }
+};
